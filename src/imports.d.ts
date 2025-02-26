d53199734873ea3da94acad49d003e0588aa5808
Vladimir Bolshakov <877240+vovanbo@users.noreply.github.com>
2025-02-26 15:58:19 +0100

    chore: Add TypeScript module declarations for *.gql and *.hbs

--- src/imports.d.ts
+++ src/imports.d.ts
@@ -7,3 +7,14 @@ declare module "*.sql" {
     const content: string;
     export default content;
 }
+
+declare module "*.gql" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.hbs" {
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    const content: HandlebarsTemplateDelegate<any>;
+    export default content;
+}
